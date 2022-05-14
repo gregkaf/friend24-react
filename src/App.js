4d668b34799e50ceea7bd35fa0a410398fcb5319
gregorykafanov <gregkaf@gmail.com>
2022-05-14 17:26:14 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Slogan from 'ui/Slogan/index';
 import './css/styles.css';
 import Header from './ui/Header';
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Slogan />
     </div>
   );
 }
---
+++ src/ui/Slogan/index.jsx
@@ -0,0 +1,20 @@
+const Slogan = () => {
+  return (
+    <section class="slogan">
+      <div class="slogan__wrapper">
+        <div class="container">
+          <div class="slogan__inner">
+            <div class="slogan__text">
+              Always on-hand to have a quick chat whenever and whenever you need
+              — day or night, AI Partner can help you through difficult moments
+              and be up to any of your romantic ideas. Get the romantic partner
+              in your pocket work with you to add colors to your life!
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Slogan;
